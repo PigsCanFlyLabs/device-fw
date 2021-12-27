59278c5fa16a8877ee04910dd025e17897fc5712
Holden Karau <holden@pigscanfly.ca>
2021-12-27 13:04:15 -0800

    Less apt spam to logs during GHA

--- common.sh
+++ common.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -ex
 
-(dpkg -l |grep gcc-arm-none-eabi) || sudo apt-get install -y build-essential libreadline-dev libffi-dev git pkg-config gcc-arm-none-eabi libnewlib-arm-none-eabi
-if ! command -v virtualenv &> /dev/null; then
-  sudo apt-get install -y python3-virtualenv
-fi
+(((dpkg -l |grep gcc-arm-none-eabi) || sudo apt-get update; sudo apt-get install -y build-essential libreadline-dev libffi-dev git pkg-config gcc-arm-none-eabi libnewlib-arm-none-eabi python3-virtualenv) &> apt.logs) || (cat apt.logs; exit 1)
 
 
 export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
