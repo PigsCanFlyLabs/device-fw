85c48012da0e279439df3883551712fbf785779e
Holden Karau <holden@pigscanfly.ca>
2022-03-11 07:21:49 -0800

    Update the SPACEBEAVER board.

--- boards/SPACEBEAVER_C3/mpconfigboard.cmake
+++ boards/SPACEBEAVER_C3/mpconfigboard.cmake
@@ -3,7 +3,7 @@ set(IDF_TARGET esp32c3)
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.ble
-    boards/GENERIC_C3_USB/sdkconfig.board
+    boards/SPACEBEAVER_C3/sdkconfig.board
 )
 
 if(NOT MICROPY_FROZEN_MANIFEST)
--- boards/SPACEBEAVER_C3/mpconfigboard.h
+++ boards/SPACEBEAVER_C3/mpconfigboard.h
@@ -1,6 +1,6 @@
 // This configuration is for a generic ESP32C3 board with 4MiB (or more) of flash.
 
-#define MICROPY_HW_BOARD_NAME               "Spacebeaver alpha0 - ESP32C3"
+#define MICROPY_HW_BOARD_NAME               "ESP32C3 module"
 #define MICROPY_HW_MCU_NAME                 "ESP32C3"
 
 #define MICROPY_HW_ENABLE_SDCARD            (0)
